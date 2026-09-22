bffc59ab876f32943c22cf7e703797e42b9826ba
Marta Đukić <martadukic2004@gmail.com>
2026-09-22 19:42:10 +0200

    Upogonjen useNavigate iz react-routera

--- src/components/Izbornik.jsx
+++ src/components/Izbornik.jsx
@@ -2,9 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { IME_APLIKACIJE } from '../constants';
+import { IME_APLIKACIJE, RouteNames } from '../constants';
+import { useNavigate } from 'react-router-dom';
 
 export default function Izbornik() {
+
+    const navigate = useNavigate()
 
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
@@ -13,13 +16,24 @@ export default function Izbornik() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Početna</Nav.Link>
+                        <Nav.Link
+                            onClick={() => { navigate(RouteNames.HOME) }}
+                        >Početna</Nav.Link>
                         <NavDropdown title="Putovanja" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Europa</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
+                            <NavDropdown.Item
+                                onClick={() => { navigate(RouteNames.PUTOVANJA) }}
+                            >Europa</NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => { navigate(RouteNames.PUTOVANJA) }}
+                            >
                                 Azija
                             </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Amerika</NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => { navigate(RouteNames.PUTOVANJA) }}
+                            >Amerika</NavDropdown.Item>
+                            <NavDropdown.Item
+                                onClick={() => { navigate(RouteNames.PUTOVANJA) }}
+                            >Australija</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
